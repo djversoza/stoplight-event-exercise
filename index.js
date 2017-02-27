359c746c3e3e0acd27297c3796013cca0d405929
DJ Versoza <djversoza@gmail.com>
2017-02-27 14:07:42 -0700

    adding files

--- index.js
+++ index.js
@@ -2,4 +2,15 @@
   'use strict';
 
   // YOUR CODE HERE
+  $(document).ready(function() {
+    $("#stopButton").click(function() {
+      $("#stopLight").toggleClass("stop")
+    })
+    $("#slowButton").click(function() {
+      $("#slowLight").toggleClass("slow")
+    })
+    $("#goButton").click(function() {
+      $("#goLight").toggleClass("go")
+    })
+  })
 })();
